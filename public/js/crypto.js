85f443cd703cde2b7bea8513ed6627518e98e847
Georg Sieber <it@georg-sieber.de>
2024-04-15 08:59:02 +0200

    fix special chars

--- public/js/crypto.js
+++ public/js/crypto.js
@@ -33,20 +33,10 @@ function base64StringToArrayBuffer(b64str) {
 }
 
 function textToArrayBuffer(str) {
-	var buf = unescape(encodeURIComponent(str)); // 2 bytes for each char
-	var bufView = new Uint8Array(buf.length);
-	for(var i=0; i < buf.length; i++) {
-		bufView[i] = buf.charCodeAt(i);
-	}
-	return bufView;
+	return new TextEncoder().encode(str);
 }
 function arrayBufferToText(arrayBuffer) {
-	var byteArray = new Uint8Array(arrayBuffer);
-	var str = '';
-	for(var i=0; i<byteArray.byteLength; i++) {
-		str += String.fromCharCode(byteArray[i]);
-	}
-	return str;
+	return new TextDecoder().decode(arrayBuffer);
 }
 
 function convertBinaryToPem(binaryData, label=null) {
